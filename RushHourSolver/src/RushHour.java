0080a6290b04d773e73aa043e32b3fafca2933f0
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-04 13:17:58 -0700

    add comment

--- RushHourSolver/src/RushHour.java
+++ RushHourSolver/src/RushHour.java
@@ -3,7 +3,7 @@ public class RushHour {
     public Class Solver{
 
 
-
+    	//Hello World - rafaelg
         //methods
 //        public void solveFromFile(String inputpath,String outputpath){
 //            //instantiate relevant classes
