b2314b821843273b60f8650f5e2739335bbd9e3d
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-06 20:31:39 -0700

    add board files

--- RushHourSolver/src/Test.java
+++ RushHourSolver/src/Test.java
@@ -1,2 +1,15 @@
 public class Test {
+
+    public static void main(String[] args) throws Exception {
+
+        Board testboard = new Board("A00.txt");
+
+        testboard.print_matrix();
+
+        testboard.print_properties();
+
+    }
+
+
+
 }
