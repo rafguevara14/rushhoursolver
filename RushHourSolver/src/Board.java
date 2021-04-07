e6fcc8e0ddd204c1f987faa258bf1114acea89aa
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-06 20:21:32 -0700

    recreated Board constructor

--- RushHourSolver/src/Board.java
+++ RushHourSolver/src/Board.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Board {
 
@@ -7,17 +10,144 @@ public class Board {
 
     private Map<String,int[]> properties = new HashMap<>();
 
+
+    private final int LENGTH = 0;
+
+    private final int ISVERT = 1; //1 is vertical
+
     //methods
 
     //sets matrix
-//    private void readFromFile();
-
-    Board(String inputpath){
+    Board(String inputpath) throws FileAlreadyExistsException {
 
         //creates the board
-//        readFromFile();
+        File file = new File(inputpath);
+
+        Scanner scanner = new Scanner(file);
+
+
+        if(!file.exists())
+            throw new FileAlreadyExistsException("Could not find file: " + inputpath);
+
+        //iterate through file and store elements in 2D matrix
+
+        String row;
+
+        while(scanner.hasNext()){
+
+            for(int i = 0; i < 6; i++){
+
+                row = scanner.next();
+
+                for(int j = 0; j < 6; j++){
+
+                    this.matrix[i][j] = String.valueOf(row.charAt(j));
+
+                }//inner loop
+
+            }//outer loop
+
+
+        }//file iteration
+
+
+//        int[] tmp_properties = new int[2];
+
+
+        //scan through entire matrix
+
+        for(int i = 0; i < 6; i++){
+
+            for(int j = 0; j < 6; j++){
+
+                int[] tmp_properties = new int[2];
+
+                //push car onto properties
+
+                if(!properties.containsKey(this.matrix[i][j]) && !this.matrix[i][j].equals(".")) {
+//                    properties.put(this.matrix[i][j],tmp_properties);
+
+
+                    //edge cases
+
+                    //determine orientation
+
+                    //check a direction that is within board
+                    int pivot = 1;
+
+                    //at edge of board, pivot to check negative direction instead
+                    if (j == 5)
+                        pivot *= -1;
+                    if (i == 5)
+                        pivot *= -1;
+
+
+                    //check vertical
+
+                    tmp_properties[ISVERT] = (this.matrix[i][j + pivot].equals(this.matrix[i][j])) ? 1 : 0;
+
+
+
+                    //determine length
+
+                    String carName = this.matrix[i][j];
+
+                    int ipos = i;
+
+                    int jpos = j;
+
+                    //positive end of car
+                    while(this.matrix[ipos][jpos] == carName){
+
+                        if(tmp_properties[ISVERT] == 0)
+                            ipos++;
+                        else
+                            jpos++;
+
+
+                    }
+
+                    int ineg = i;
+
+                    int jneg = j;
+
+                    //negative end of car
+                    while(this.matrix[ineg][jneg] == carName){
+
+                        if(tmp_properties[ISVERT] == 0)
+                            ineg--;
+                        else
+                            jneg--;
+                    }
+
+                    //horizontal
+                    if(tmp_properties[ISVERT] == 0){
+
+                        tmp_properties[LENGTH] = (ipos - ineg) + 1;
+                    }else
+                        tmp_properties[LENGTH] = (jpos - jneg) + 1;
+
+
+                    //finally, push car onto properties array
+
+                    properties.put(this.matrix[i][j],tmp_properties);
+
+
+
+                }
+
+
+            }//inner loop
+
+        }//outer loop
+
+
+
+
 
         //gets length of every car
+
+
 
 
         //gets orientation
