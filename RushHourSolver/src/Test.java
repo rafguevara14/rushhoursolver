0f9b3236d1267e471c3a165d286369d5d18866f3
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-08 13:52:18 -0700

    add test cases for ROM logic

--- RushHourSolver/src/Test.java
+++ RushHourSolver/src/Test.java
@@ -5,7 +5,61 @@ public class Test {
 
         Node test = new Node();
 
+
+        //[0,0]
+        test.ROMtest("O",5,0,"A00.txt");
+
+        //[0,3]
+        test.ROMtest("X",0,2,"A00.txt");
+
+
+        //[0,3]
         test.ROMtest("A",0,0,"A00.txt");
+
+
+        //[-3,0]
+        test.ROMtest("Q",3,3,"A00.txt");
+
+        //[-4,0]
+        test.ROMtest("C",4,4,"A00.txt");
+
+        //[-2,1]
+        test.ROMtest("R",2,5,"A00.txt");
+
+
+        //[0,0]
+        test.ROMtest("A",0,0,"B16.txt");
+
+        //[0,0]
+        test.ROMtest("B",2,0,"B16.txt");
+
+
+        //[0,2]
+        test.ROMtest("D",0,1,"B16.txt");
+
+
+        //[-1,0]
+        test.ROMtest("E",2,1,"B16.txt");
+
+        //[-1,1]
+        test.ROMtest("F",1,3,"B16.txt");
+
+        //[0,4]
+        test.ROMtest("G",0,5,"B16.txt");
+
+        //[0,1]
+        test.ROMtest("P",2,2,"B16.txt");
+
+
+        //[0,0]
+        test.ROMtest("Q",3,3,"B16.txt");
+
+
+        //[0,0]
+        test.ROMtest("X",3,2,"B16.txt");
+
+
+
 
 
 
