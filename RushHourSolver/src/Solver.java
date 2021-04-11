6d7943fc72ff9fdf9512dabfe160443162cf4330
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-10 18:52:02 -0700

    restructured classes

---
+++ RushHourSolver/src/Solver.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
+public class Solver {
+
+
+
+    private static String Astar(Node initVertex){
+
+
+
+        return
+    }
+
+    //methods
+    public static void solveFromFile(String inputPath,String outputPath) throws Exception {
+        //instantiate relevant classes
+
+
+        //reads from file
+        Node initVertex = new Node(inputPath);
+
+        outputPath = Astar(initVertex);
+
+
+
+
+
+    }//solveFromFile
+
+
+
+
+    } //Solver
+
