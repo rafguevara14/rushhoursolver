870a21e8950e532fc32d69349aa711702d7f858e
rafguevara14 <[rafaelg@sfu.ca]>
2021-04-16 17:04:40 -0700

    clean Solver code

--- RushHourSolver/src/Solver.java
+++ RushHourSolver/src/Solver.java
@@ -8,166 +8,94 @@ import java.util.*;
 
 public class Solver {
 
+    public static void solveFromFile(String inputPath,String outputPath) throws Exception {
 
-    public static void testClosedSet()throws Exception{
+        //reads from file
+        Node initVertex = new Node(inputPath);
 
-        HashSet<Node> ClosedSet = new HashSet<Node>();
+        //run A* algorithm
+        Node solutionNode = Astar(initVertex);
 
-
-        Node B = new Node("A00.txt");
-
-        ClosedSet.add(B);
-
-        System.out.println("B");
-
-
-        B.getBoard().print_matrix();
-
-        System.out.println();
-
-        Node A = new Node("A",2,B);
-
-
-        System.out.println("A");
-
-
-        System.out.println("Hash code of A: " + A.hashCode());
-
-        A.getBoard().print_matrix();
-
-        System.out.println();
-
-        ClosedSet.add(A);
-
-
-        Node middleman = new Node("A",1,A);
-
-        System.out.println("MiddleMan");
-
-        middleman.getBoard().print_matrix();
-
-
-        System.out.println();
-
-        ClosedSet.add(middleman);
-
-
-        Node A2 = new Node("A",-1,middleman);
-
-        System.out.println("A2");
-
-
-        System.out.println("Hash code of A2: " + A2.hashCode());
-
-
-        A2.getBoard().print_matrix();
-
-        System.out.println();
-
-
-        //should both be true
-        System.out.println("Is A2 already in the closed set? " + ClosedSet.contains(A2));
-
-        System.out.println("Is A2 equal to A? " + A.equals(A2));
-
-
-        //A and A2 have the same boards, but traversed in different ways
-        //hashCode should be the same
+        //output solution moves to textfile
+        String moveList = createMoveList(solutionNode);
+        sendtoFile(outputPath,moveList);
 
     }
 
+    //    actual A* algo. takes initial node and returns solution node
+    private static Node Astar(Node initVertex){
 
-    public static void testClosedMap()throws Exception{
+        //initialize data structures
+        PriorityQueue<Node> OpenQueue = new PriorityQueue<Node>();
 
         HashMap<Integer,Node> ClosedSet = new HashMap<Integer,Node>();
 
+        OpenQueue.add(initVertex);
 
-        Node B = new Node("A00.txt");
+        //A* algorithm
+        while(!OpenQueue.isEmpty()){
 
-        ClosedSet.put(B.hashCode(),B);
+            Node currentNode = OpenQueue.remove();
 
-        System.out.println("B");
+            if(ClosedSet.containsKey(currentNode.hashCode()))
+                continue;
 
+            //go through currentNode's neighbours
+            for(Node neighbour : currentNode.generateNeighbours()){
 
-        B.getBoard().print_matrix();
+                if(ClosedSet.containsKey(neighbour.hashCode()))
+                    continue;
 
-        System.out.println();
+                //solved board
+                if(neighbour.isSolved()) {
 
-        Node A = new Node("A",2,B);
+//                    neighbour.getBoard().print_matrix();
 
+                    neighbour.setParent(currentNode);
 
-        System.out.println("A");
+                    System.out.println("Found Solution!\n");
 
+                    return neighbour;
 
-        System.out.println("Hash code of A: " + A.hashCode());
+                }else{
 
-        A.getBoard().print_matrix();
+                    OpenQueue.add(neighbour);
 
-        System.out.println();
+                    neighbour.setParent(currentNode);
+                }
 
-        ClosedSet.put(A.hashCode(),A);
+            }// for loop
 
+            //mark as visited
+            ClosedSet.put(currentNode.hashCode(),currentNode);
 
-        Node middleman = new Node("A",1,A);
+        }//while loop
 
-        System.out.println("MiddleMan");
+        System.out.println("NO SOLUTION FOUND :(");
 
-        middleman.getBoard().print_matrix();
-
-
-        System.out.println();
-
-        ClosedSet.put(middleman.hashCode(),middleman);
-
-
-        Node A2 = new Node("A",-1,middleman);
-
-        System.out.println("A2");
-
-
-        System.out.println("Hash code of A2: " + A2.hashCode());
-
-
-        A2.getBoard().print_matrix();
-
-        System.out.println();
-
-
-        //should both be true
-        System.out.println("Is A2 already in the closed set? " + ClosedSet.containsKey(A2.hashCode()));
-
-        System.out.println("Is A2 equal to A? " + A.equals(A2));
-
-
-        //A and A2 have the same boards, but traversed in different ways
-        //hashCode should be the same
-
+        return null;
     }
-
-
-
 
     private static String createMoveList(Node node){
 
         String moveList = "";
         Stack<String> stack = new Stack<String>();
 
-//        while node has parent, get the move and store it in stack
-        while (node.getParent() != null) {
+        //push moves from end to beginning onto stack
+        while(node != null){
+
             stack.push(node.getMove());
-            //traverse up
+
+            //traverse 'up' graph
             node = node.getParent();
-//            node.setParent(node.getParent());
         }
 
 
-//        pop from stack and concatenate to a String
+        //pop stack and concatenate to string
         while (!stack.isEmpty())
             moveList = moveList.concat(stack.pop());
 
-
         return moveList;
-
     }
 
     private static void sendtoFile(String outputPath,String moveList) {
@@ -183,172 +111,129 @@ public class Solver {
             e.printStackTrace();
         }
     }
+
+    /* static test functions */
+
+    public static void testClosedSet()throws Exception{
+
+        HashSet<Node> ClosedSet = new HashSet<Node>();
+
+        Node B = new Node("A00.txt");
+
+        ClosedSet.add(B);
+
+        System.out.println("B");
+
+        B.getBoard().print_matrix();
+
+        System.out.println();
+
+        Node A = new Node("A",2,B);
+
+        System.out.println("A");
+
+        System.out.println("Hash code of A: " + A.hashCode());
+
+        A.getBoard().print_matrix();
+
+        System.out.println();
+
+        ClosedSet.add(A);
+
+        Node middleman = new Node("A",1,A);
+
+        System.out.println("MiddleMan");
+
+        middleman.getBoard().print_matrix();
+
+        System.out.println();
+
+        ClosedSet.add(middleman);
+
+        Node A2 = new Node("A",-1,middleman);
+
+        System.out.println("A2");
+
+        System.out.println("Hash code of A2: " + A2.hashCode());
+
+        A2.getBoard().print_matrix();
+
+        System.out.println();
+
+        //should both be true
+        System.out.println("Is A2 already in the closed set? " + ClosedSet.contains(A2));
+
+        System.out.println("Is A2 equal to A? " + A.equals(A2));
+
+        //A and A2 have the same boards, but traversed in different ways
+        //hashCode should be the same
+    }
+
+    public static void testClosedMap()throws Exception{
+
+        HashMap<Integer,Node> ClosedSet = new HashMap<Integer,Node>();
+
+        Node B = new Node("A00.txt");
+
+        ClosedSet.put(B.hashCode(),B);
+
+        System.out.println("B");
+
+        B.getBoard().print_matrix();
+
+        System.out.println();
+
+        Node A = new Node("A",2,B);
+
+        System.out.println("A");
+
+        System.out.println("Hash code of A: " + A.hashCode());
+
+        A.getBoard().print_matrix();
+
+        System.out.println();
+
+        ClosedSet.put(A.hashCode(),A);
+
+        Node middleman = new Node("A",1,A);
+
+        System.out.println("MiddleMan");
+
+        middleman.getBoard().print_matrix();
+
+        System.out.println();
+
+        ClosedSet.put(middleman.hashCode(),middleman);
+
+        Node A2 = new Node("A",-1,middleman);
+
+        System.out.println("A2");
+
+        System.out.println("Hash code of A2: " + A2.hashCode());
+
+        A2.getBoard().print_matrix();
+
+        System.out.println();
+
+        //should both be true
+        System.out.println("Is A2 already in the closed set? " + ClosedSet.containsKey(A2.hashCode()));
+
+        System.out.println("Is A2 equal to A? " + A.equals(A2));
+
+        //A and A2 have the same boards, but traversed in different ways
+        //hashCode should be the same
+    }
+
     public static void moveListTest(String file) throws Exception {
 
-
-//        createMoveList testing:
+        //createMoveList testing:
         Node initNode = new Node(file);
-
-
 
         System.out.println("Old Board\n");
         initNode.getBoard().print_matrix();
 
-
-
-
         System.out.println(Astar(initNode));
-
-//        Node node2 = new Node("X", 2, initNode);
-//        Node node3 = new Node("X", 1, node2);
-
-//        should return :
-//        XR2
-//        XR1
-//        Solver.moveListTest(node2);
-
-
-
     }
-
-
-
-//    actual A* algo. takes initial node and returns solution node
-    private static Node Astar(Node initVertex){
-
-//        String[][] answer={ {".",".",".",".","A","A"},
-//                            {".",".",".",".",".","."},
-//                            {".",".",".",".","X","X"},
-//                            {".",".","Q","Q","Q","O"},
-//                            {"C","C",".",".",".","O"},
-//                            {".","R","R","R",".","O"},
-//
-//
-//                        };
-
-
-//        ....AA
-//        ......
-//        ....XX
-//        ..QQQO
-//        CC...O
-//        .RRR.O
-
-//        int count= 0;
-
-        //initialize data structures
-        PriorityQueue<Node> OpenQueue = new PriorityQueue<Node>(); //override comparator for heuristic implementation
-
-//        HashSet<Node> ClosedSet = new HashSet<Node>();
-
-        HashMap<Integer,Node> ClosedSet = new HashMap<Integer,Node>();
-
-        OpenQueue.add(initVertex);
-
-        //A* algorithm
-        while(!OpenQueue.isEmpty()){
-
-            Node currentNode = OpenQueue.remove();
-
-//            if(OpenQueue.isEmpty()){
-//
-//                System.out.print("almost done!\n");
-//            }
-//
-//            if(Arrays.deepEquals(currentNode.getBoard().getMatrix(), answer)){
-//
-//                System.out.println("Solution found!");
-//            }
-
-            if(ClosedSet.containsKey(currentNode.hashCode()))
-                continue;
-
-            //go through currentNode's neighbours
-
-            for(Node neighbour : currentNode.generateNeighbours()){
-
-                if(ClosedSet.containsKey(neighbour.hashCode()))
-                    continue;
-
-
-                //solved board
-                if(neighbour.isSolved()) {
-
-//                    neighbour.getBoard().print_matrix();
-
-                    neighbour.setParent(currentNode);
-
-//                    createMoveList(currentNode);
-
-
-//                    System.out.println("Found Solution!\n");
-
-                    return neighbour;
-//                  return createFilePath(neighbour);
-
-                }else{
-
-
-                    OpenQueue.add(neighbour);
-
-                    neighbour.setParent(currentNode);
-                }
-
-
-//                count++;
-
-            }// for loop
-
-//            if(count == 38494){
-//
-//                currentNode.getBoard().print_matrix();
-//
-//
-//
-//                System.out.println("Hi");
-//            }
-
-            //mark as visited
-            ClosedSet.put(currentNode.hashCode(),currentNode); //consider where to add stuff to the closed set
-
-        }//while loop
-
-//        System.out.println("Count: " + count);
-
-        System.out.println("NO SOLUTION FOUND :(");
-
-        return null;
-    }
-
-    //methods
-    public static void solveFromFile(String inputPath,String outputPath) throws Exception {
-        //instantiate relevant classes
-
-
-        //reads from file
-        Node initVertex = new Node(inputPath);
-
-//        System.out.println("Old Board\n");
-
-//        initVertex.getBoard().print_matrix();
-
-//        System.out.println(Astar(initVertex));
-
-
-        Node solutionNode = Astar(initVertex);
-
-        String moveList = createMoveList(solutionNode);
-
-        sendtoFile(outputPath,moveList);
-
-          //testing code
-//        System.out.println(Astar(initVertex));
-
-        //makes an empty output file called outputPath
-//        File outputFile = new File(outputPath);
-
-
-    }//solveFromFile
 
 
 
